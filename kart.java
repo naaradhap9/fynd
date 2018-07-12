79d22a735b6a0666dee3071e4977dccbf50d891e
naaradhap9 <naaradhap9@gmail.com>
2018-07-12 15:50:33 +0530

    kart code

---
+++ kart.java
@@ -0,0 +1,21 @@
+="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
+  <modelVersion>4.0.0</modelVersion>
+  <groupId>com.kart</groupId>
+  <artifactId>kart</artifactId>
+  <packaging>war</packaging>
+  <version>1.50-SNAPSHOT</version>
+  <name>kart Maven Webapp</name>
+  <url>http://maven.apache.org</url>
+  <dependencies>
+    <dependency>
+      <groupId>junit</groupId>
+      <artifactId>junit</artifactId>
+      <version>3.8.1</version>
+      <scope>test</scope>
+    </dependency>
+  </dependencies>
+  <build>
+    <finalName>kart</finalName>
+  </build>
+</project>
