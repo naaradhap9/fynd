07f1f6b8f23ce16d85d5167046fe566198b6ab3b
naaradhap9 <naaradhap9@gmail.com>
2018-07-16 11:12:37 +0530

    updated

--- kart.java
+++ kart.java
@@ -1,6 +1,7 @@
 ="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
-  <modelVersion>4.0.0</modelVersion>
+  
+<modelVersion>4.0.0</modelVersion>
   <groupId>com.kart</groupId>
   <artifactId>kart</artifactId>
   <packaging>war</packaging>
@@ -18,4 +19,5 @@
   <build>
     <finalName>kart</finalName>
   </build>
+
 </project>
